1a0382238ae5f02d339bcb376f9e233fe5910966
Helmy Bouchiha <helmibouchiha@gmail.com>
2023-06-07 14:06:20 +0100

    adding documentation

--- lib/src/custom_will_pop_scope.dart
+++ lib/src/custom_will_pop_scope.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
+/// An alternative of [WillPopScope] that enable the iOS swipe to return.
 class CustomWillPopScope extends StatelessWidget {
-  final Widget child;
-  final bool canReturn;
-  final Future<bool> Function() onWillPop;
-  final void Function() onPopAction;
-
+  /// Creates a widget that registers a callback to veto attempts by the user to
+  /// dismiss the enclosing [ModalRoute].
+  ///
+  /// The `child`,`onWillPop` and `onPopAction` arguments must not be `null`.
   const CustomWillPopScope({
     Key? key,
     required this.child,
@@ -14,10 +14,25 @@ class CustomWillPopScope extends StatelessWidget {
     required this.onPopAction,
   }) : super(key: key);
 
+  /// The widget below this widget in the tree.
+  final Widget child;
+
+  /// Whether the navigator that most tightly encloses the given context can be
+  /// popped.
+  final bool canReturn;
+
+  ///Called to veto attempts by the user to dismiss the enclosing [ModalRoute].
+  ///If the callback returns a Future that resolves to false, the enclosing route will not be popped.
+  final Future<bool> Function() onWillPop;
+
+  /// Called when dissmissing the enclosing [ModalRoute].
+  final void Function() onPopAction;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onPanEnd: (details) {
+        // Enables the iOS return swipe according to some conditions
         if (canReturn &&
             (details.velocity.pixelsPerSecond.dx < 0 ||
                 details.velocity.pixelsPerSecond.dx > 0) &&
